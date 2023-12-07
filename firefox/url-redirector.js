6a4fb53c87612dc5df3f430d6ac8aa3f96981d43
Anmol <bhullar_anmol@live.ca>
2023-12-07 14:39:56 -0500

    created+tested the extension for firefox. Don't know if I needed to convert the service worker to be a background script, but I did it anyway.

---
+++ firefox/url-redirector.js
@@ -0,0 +1,31 @@
+/**
+   File: url-redirector.js,
+   
+   Description: Redirects any pathofexile.fandom.com links to poewiki.net. In my opinion, the
+   former has outdated information, and the latter wiki is more frequently updated as well as
+   being hosted by the publisher/developers of path of exile (Grinding Gear Games). This is
+   a service worker which means it has its own environment in the browser that it runs in,
+   independent of any tab or window. Thus, it should work even if you open the aforementioned
+   link in a new tab or window.
+
+   It uses the webNavigation API, the script is not persistent (aka it does not run all the
+   time, only when the event listener is called (aka an onBeforeNavigate event for a link
+   that contains the aforementioned website).
+*/
+
+browser.webNavigation.onBeforeNavigate.addListener(function(details) {
+    if (details.url.includes("pathofexile.fandom.com")) {
+	// debugging
+	console.log('onBeforeNavigate ', details.tabId, details.url);
+	var baseRedirectUrl = "https://www.poewiki.net";
+
+	// get the path to the specific page they're going to
+	var path = details.url.split("pathofexile.fandom.com")[1];
+
+	// now construct full url we're redirecting to
+	var redirectUrl = baseRedirectUrl + path;
+
+	// redirect
+	browser.tabs.update(details.tabId, {url: redirectUrl});
+    }
+}, {url: [{urlMatches : 'pathofexile.fandom.com'}]});
